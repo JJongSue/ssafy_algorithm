5bfa3bfb21685247fd4814b119a187d1b0ea2502
박종수 <jspark940321@gmail.com>
2019-10-01 15:50:14 +0900

    adf

--- Problem/src/boj/Main10026.java
+++ Problem/src/boj/Main10026.java
@@ -1,5 +1,13 @@
 package boj;
 
-public class Main10026 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main10026 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+	}
 }
