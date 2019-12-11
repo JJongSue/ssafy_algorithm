fdbdd332b22180693102a6f6c95cb382c4918633
JJongSue <jspark940321@gmail.com>
2019-12-11 23:20:29 +0900

    adfaf

--- Problem/src/B형/Solution3947_2.java
+++ Problem/src/B형/Solution3947_2.java
@@ -5,45 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution3947 {
+public class Solution3947_2 {
 	static int[] parents;
 	static priorityQueue pq;
+	static ArrayList al[];
 	
 	static class road{
 		int x;
-		int y;
 		int dis;
-		public road(int x, int y, int dis) {
+		public road(int x, int dis) {
 			super();
 			this.x = x;
-			this.y = y;
 			this.dis = dis;
 		}		
 	}
 	
-	static int find_set(int x) {
-		if(parents[x] < 0) return x;
-		return parents[x] = find_set(parents[x]);
-	}
-	static void union(int x, int y) {
-		int px = find_set(x);
-		int py = find_set(y);
-		if(px != py) {
-			if(parents[px] < parents[py]) {
-				parents[px] += parents[py];
-				parents[py] = px;				
-			}else {
-				parents[py] += parents[px];
-				parents[px] = py;
-			}
-		}
-	}
 	
 	static class priorityQueue{
 		road pq[];
 		int now;
 		public priorityQueue() {
-			pq = new road[500001];
+			pq = new road[1000001];
 		}
 		
 		void add(road r) {
@@ -91,6 +73,34 @@ public class Solution3947 {
 		}
 		
 	}
+	static class ArrayList{
+		road pair[];
+		int now = 0;
+		int size;
+		public ArrayList() {
+			size = 2;
+			pair = new road[size];
+		}
+		void add(road x) {
+			if(now == size) {
+				road tmpp[] = new road[size];
+				for(int i=0;i<size;i++) {
+					tmpp[i] = pair[i];									
+				}
+				pair = new road[size+size/2];
+				for(int i=0;i<size;i++) {
+					pair[i] = tmpp[i];				
+				}
+				size += (size/2);
+			}
+			pair[now] = x;
+			
+			now++;
+		}
+		road get(int x) {
+			return pair[x];
+		}
+	}
 	
 	
 	
@@ -106,39 +116,46 @@ public class Solution3947 {
 			int M = Integer.parseInt(st.nextToken());
 			parents = new int[N+1];
 			pq = new priorityQueue();
-			long ans = 0;
-			for(int i=1;i<=N;i++) parents[i] = -1;
+			al = new ArrayList[N+1];
+			for(int i=1;i<=N;i++) {
+				parents[i] = -1;
+				al[i] = new ArrayList();
+			}
+			
 			
 			for(int i=0;i<M;i++) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				int dis = Integer.parseInt(st.nextToken());
-				if(x == 1 || y==1) {
-					parents[x] = 1;
-					parents[y] = 1;
-					ans +=dis;
-				}
-				else pq.add(new road(x, y, dis));
+				al[x].add(new road(y, dis));
+				al[y].add(new road(x, dis));
+				//pq.add(new road(x, y, dis));
 			}
-			parents[1] = -1;
+			long ans = 0;
+			pq.add(new road(1, 0));
 			while(!pq.is_empty()) {
 				int cnt = 0;
 				for(int i=1;i<=N;i++) {
-					if(parents[find_set(i)] < 0) cnt++;
+					if(parents[i] < 0) cnt++;
 					if(cnt>1) break;
 				}
-				if(cnt == 1) break;
-				
+				if(cnt == 0) break;
 				int x = pq.pq[0].x;
-				int y = pq.pq[0].y;
 				int dis = pq.poll().dis;
-				int px = find_set(x);
-				int py = find_set(y);
-				if(px != py) {
-					union(x, y);
-					ans+=dis;
+				if(parents[x] != -1) continue;
+				parents[x] = dis;
+				//System.out.println(x+" "+dis);
+				
+				ans +=dis;
+				for(int i=0;i<al[x].now;i++) {
+					if( parents[al[x].get(i).x] ==-1) {
+						
+						pq.add(new road(al[x].get(i).x, al[x].get(i).dis));
+						
+					}
 				}
+				
 			}
 			System.out.println("#"+tc+" "+ans);
 			
