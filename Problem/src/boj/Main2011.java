0cb3d3ebe2a50a55a168f26747e383616d4d0545
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-01-09 17:52:28 +0900

    adfadf

--- Problem/src/boj/Main2011.java
+++ Problem/src/boj/Main2011.java
@@ -12,12 +12,97 @@ public class Main2011 {
 		tmp = br.readLine();
 		dp = new int[tmp.length()][2];
 		int start = tmp.length()-1;
-		if(tmp.charAt(start) == '0') {
+		if(tmp.charAt(0) == '0') {
+			System.out.println(0);
+		}
+		else if(tmp.length() == 1) {
+			if(tmp.charAt(0) == '0') System.out.println(0);
+			else System.out.println(1);
+		}else {
+			if(tmp.charAt(start) == '0' &&tmp.charAt(start-1) > '2') {
+				System.out.println(0);
+				return;
+			}
+			if(tmp.charAt(start) == '0' &&tmp.charAt(start-1) == '0') {
+				System.out.println(0);
+				return;
+			}
+			if(tmp.charAt(start-1) == '2' && tmp.charAt(start) <= '6' && tmp.charAt(start)> '0') {
+				System.out.println(tmp);
+				dp[start-1][1] = 1;
+				dp[start-1][0] = 1;
+				dp[start][1] = 0;
+				dp[start][0] = 1;
+				
+			}else if(tmp.charAt(start-1) == '2' && tmp.charAt(start) ==  '0') {
+				System.out.println("20");
+				dp[start-1][1] = 1;
+				dp[start-1][0] = 0;
+				dp[start][1] = 0;
+				dp[start][0] = 0;
+			}else if(tmp.charAt(start-1) == '2') {
+				dp[start-1][1] = 0;
+				dp[start-1][0] = 1;
+				dp[start][1] = 0;
+				dp[start][0] = 1;
+			}else if(tmp.charAt(start-1) == '1' && tmp.charAt(start) == '0') {
+				dp[start-1][0] = 0;
+				dp[start-1][1] = 1;
+			}
+			else if(tmp.charAt(start-1) == '1' && tmp.charAt(start) != '0') {
+				dp[start-1][1] = 1;
+				dp[start-1][0] = 1;
+				dp[start][1] = 0;
+				dp[start][0] = 1;
+			}
+			else if(tmp.charAt(start-1) == '0') {
+				
+				dp[start][0] = 1;
+			}
+			else {
+				dp[start][0] = 1;
+				dp[start-1][0] = 1;
+			}
+//			if(tmp.charAt(start) == '0' && tmp.charAt(start-1) == '0') {
+//				System.out.println(0);
+//				return;
+//			}
+			start = start -2;
+			for(int i=start;i>=0;i--) {
+				char now = tmp.charAt(i);
+				if(now == '0' && tmp.charAt(i+1) == '0') {
+					dp[0][0] =0;
+					dp[0][1] = 0;
+					break;
+				}
+				if(now == '0') {
+					continue;
+				}
+				if(now == '1') {
+					dp[i][0] = (dp[i+1][1] + dp[i+1][0])%1000000;
+					dp[i][1] = (dp[i+2][0] + dp[i+2][1])%1000000;
+				}else if(now == '2') {
+					if(tmp.charAt(i+1) <= '6') {
+						dp[i][0] = (dp[i+1][1] + dp[i+1][0])%1000000;
+						dp[i][1] = (dp[i+2][0] + dp[i+2][1])%1000000;
+					}else {
+						dp[i][0] = (dp[i+1][1] + dp[i+1][0])%1000000;
+					}
+				}else {
+					dp[i][0] = (dp[i+1][1] + dp[i+1][0])%1000000;
+				}
+				
+			}
+			int ans = (dp[0][0] + dp[0][1]) %1000000;
+			System.out.println(ans);
 			
 		}
-		dp[tmp.length()-1][0] = 1;
-		for(int i=tmp.length()-1;i>=0;i--) {
-			
-		}
+		
+//		for(int i=0;i<tmp.length();i++) {
+//			System.out.print(dp[i][0] + " ");
+//		}System.out.println();
+//		for(int i=0;i<tmp.length();i++) {
+//			System.out.print(dp[i][1] + " ");
+//		}System.out.println();
 	}
 }
--- Problem/src/boj/Main9935.java
+++ Problem/src/boj/Main9935.java
@@ -10,21 +10,74 @@ public class Main9935 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char map[] = br.readLine().toCharArray();
 		char f[] = br.readLine().toCharArray();
-		Stack<Character> sta = new Stack<>();
+		Stack<Integer> sta = new Stack<>();
+		int cnt = 0;
 		for(int i=0;i<map.length;i++) {
-			if(map[i] == f[f.length-1]) {
-				Stack<Character> tmps = new Stack<>();
-				for(int l=f.length-1;l>=0;l--) {
-					if(sta.isEmpty()) break;
-					else {
-						if(sta.peek() == f[l])
-						tmps.add(sta.pop());
-						else {
-							break;
-						}
-					}
+			System.out.println(i+" "+cnt);
+			//int cnt = 0;
+			if(!sta.isEmpty()) {
+				int tmp = sta.peek();
+				if(tmp == f[cnt]) {
+					cnt++;
+					sta.pop();
+				}else if( tmp > 127  ) {
+					cnt = sta.pop()-127;
 					
 				}
+			}
+			if(map[i] == f[cnt]) {
+				//Stack<Character> sttmp = new Stack<>();
+				cnt++;
+				//sttmp.add(map[i]);
+				for(int j=cnt;j<f.length&&i+j<map.length;j++) {
+					if(map[i+j] == f[j]) {
+						//sttmp.add(f[j]);
+						cnt++;
+						if(cnt == f.length) {
+							i += j; 
+							cnt = 0;
+							break;
+						}
+					}else {
+						cnt = 0;
+						sta.add((cnt+127));
+						i = i + j -1;
+						break;
+					}
+				}
+				
+				
+			}
+			else {
+				if(cnt != 0) {
+					sta.add((cnt+127));
+					System.out.println("cnt : " + cnt);
+					cnt = 0;
+				}
+				if(map[i] == f[0]) {
+					System.out.println("same" + map[i]);
+					cnt = 1;
+				}else {
+					System.out.println("input" + map[i]);
+					sta.add((int) map[i]);
+				}
+			}
+			
+		}
+		Stack<Integer> tmps = new Stack<>();
+		while(!sta.isEmpty()) {
+			tmps.add(sta.pop());
+		}
+		while(!tmps.isEmpty()) {
+			int nums = tmps.pop();
+			//System.out.println(nums);
+			if(nums>128) {
+				for(int i=0;i<nums-127;i++) {
+					System.out.print(f[i]);
+				}
+			}else {
+				char ch = (char) nums;
+				System.out.print(ch);
 			}
 		}
 	}
